67c9b93edf716f046ba7a01d389e9ed460adb921
foobar <none@nowhere.no>
2014-06-19 00:06:25 +0200

    astyle mit tabs?

--- cool.c
+++ cool.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-int main (void)
-{
+int main (void) {
     false && printf("hello, world\n") || printf("hello, stupid\n");
     return 0;
 }
--- uncool.c
+++ uncool.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
-int main (void)
-{
+int main (void) {
     if (true) {
-        printf("hello, world\n");
+	printf("hello, world\n");
     } else {
-        printf("hello, stupid\n");
+	printf("hello, stupid\n");
     }
     return 0;
 }
