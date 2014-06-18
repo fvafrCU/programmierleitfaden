d10fb59cca3ccfbb260f05fd4635c738c833bcb4
foobar <none@nowhere.no>
2014-06-18 23:15:51 +0200

    squash double blanks

--- install_roxy.R
+++ install_roxy.R
@@ -1,3 +1,4 @@
-install.packages('roxygen2', repos =  'http://ftp5.gwdg.de/pub/misc/cran/')
-package <- 'H:/BuI/Persoenlich/Cullmann/programmierleitfaden/roxygen2ForSingleFiles_0.1-2.tar.gz'
-install.packages(package, repos = NULL, type = 'source')
+install.packages('roxygen2', repos = 'http://ftp5.gwdg.de/pub/misc/cran/')
+package <- 'roxygen2ForSingleFiles_0.1-2.tar.gz'
+path <- 'H:/FVA-Projekte/Methodenforum/programmierleitfaden'
+install.packages(paste(path, package, sep = '/'), repos = NULL, type = 'source')
--- sum_up.R
+++ sum_up.R
@@ -1,7 +1,7 @@
 ## I haven't yet had time to read the documentation, 
 ## so I don't have a clue if there's a
 ## standard library to do it. TODO: RTFM
-sum_up  <- function(x) {
+sum_up <- function(x) {
     ## sum up the vector's elements
     total <- 0
     i <- 1 ## vector indices in R start at 1, not at 0!
