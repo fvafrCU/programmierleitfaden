6cb9a935041dbac73cef606661d570de9d9dd1d7
Dominik Cullmann <dominik.cullmann@forst.bwl.de>
2014-06-24 13:49:14 +0200

    add Gerald's review
    * set version to 0.2-2
    * remove extraneous at-signs in comment

--- documentation.R
+++ documentation.R
@@ -3,7 +3,7 @@
 # Contents of README are written to file README.txt in the 
 # directory given via the directory parameter.
 #
-# Author: Dominik Cullmann <dominik.cullmann@@forst.bwl.de>
+# Author: Dominik Cullmann <dominik.cullmann@forst.bwl.de>
 # Version: Wed Jun  4 11:00:33 CEST 2014
 # Parameter (directory): Path of the directory to put README.txt into.
 # Return: TRUE on success, FALSE otherwise.
