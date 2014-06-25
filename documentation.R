1f130ec58273444af6764acf1938b0427d16f1e1
Dominik Cullmann <dominik.cullmann@forst.bwl.de>
2014-06-25 12:33:02 +0200

    documentation: fix keywords layout

--- documentation.R
+++ documentation.R
@@ -5,6 +5,6 @@
 #
 # Author: Dominik Cullmann <dominik.cullmann@forst.bwl.de>
 # Version: Wed Jun  4 11:00:33 CEST 2014
-# Parameter (directory): Path of the directory to put README.txt into.
+# Parameter: directory Path of the directory to put README.txt into.
 # Return: TRUE on success, FALSE otherwise.
 # Note: directory must exist and be writeable. No checks done.
