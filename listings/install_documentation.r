afc5a2dd1115b30cf90337e428de0631f922924d
Dominik Cullmann <dominik.cullmann@forst.bwl.de>
2015-04-14 10:24:23 +0200

    fix installation bug
    httr library is not loaded, need to call all functions via package::function().
    
    - listings/install_documentation.r: prefix use_proxy() by httr:.
    - VERSION: bump version

--- listings/install_documentation.r
+++ listings/install_documentation.r
@@ -3,7 +3,7 @@ installed_packages <- rownames(installed.packages())
 # machines at FVAFR need to set the proxy correctly.
 if (grepl("FVAFR-",  Sys.info()["nodename"])) {
     if (! "httr" %in% installed_packages)  install.packages("httr") 
-    httr::set_config(use_proxy(url="10.127.255.17", port=8080))
+    httr::set_config(httr::use_proxy(url="10.127.255.17", port=8080))
 }
 # documentation is a package not on CRAN, it is provided via github.
 if (! "devtools" %in% installed_packages) install.packages("devtools")
