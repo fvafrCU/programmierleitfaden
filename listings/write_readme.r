0f5fc58ec9f72a27887012c6600f52150e07eb3d
Dominik Cullmann <dominik.cullmann@forst.bwl.de>
2015-04-14 10:46:02 +0200

    minor change: make comment more verbose

--- listings/write_readme.r
+++ listings/write_readme.r
@@ -1,8 +1,8 @@
 #!/usr/bin/Rscript --vanilla
 #' Write the contents of the global variable \code{README} to disk
 #'
-#' Contents of \code{README} are written to file README.txt in the directory 
-#' given via the \code{directory} parameter.
+#' Contents of the global variable \code{README} are written to file README.txt 
+#' in the directory given via the \code{directory} parameter.
 #'
 #' @author Dominik Cullmann <dominik.cullmann@@forst.bwl.de>
 #' @section Version: $Id$
@@ -12,10 +12,10 @@
 write_readme <- function(directory) {
     is_success <- FALSE
     if (exists('README') == TRUE) {
-	cat(README, file = file.path(directory, 'README.txt'))
-	is_success <- TRUE
+        cat(README, file = file.path(directory, 'README.txt'))
+        is_success <- TRUE
     } else {
-	warning('README does not exist!')
+        warning('README does not exist!')
     }
     return(invisible(is_success))
 }
