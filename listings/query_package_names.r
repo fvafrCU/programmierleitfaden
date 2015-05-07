25c59b5f7f5542bee774758fd922d3ffd7b5d06f
fvafrCU <dominik.cullmann@forst.bwl.de>
2015-05-07 23:36:19 +0200

    clean up TODOs
    add a link to agplv3 and make query_package_names.r easier to understand

--- listings/query_package_names.r
+++ listings/query_package_names.r
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript --vanilla
 ## query ratio of lowerCamelCase to UpperCamelCase package names on CRAN.
 options(repos=structure(c(CRAN="http://ftp5.gwdg.de/pub/misc/cran/")))
-package_names <- as.character(available.packages()[,1])
+package_names <- row.names(available.packages())
 lowerCamelCase <- grep('^[a-z]*[a-z,0-9,.]*[A-Z][a-z,0-9,.]*', package_names)
 UpperCamelCase <- grep('^[A-Z][a-z,0-9,.]*[A-Z][a-z,0-9,.]*', package_names)
 print(length(lowerCamelCase) / length(UpperCamelCase))
