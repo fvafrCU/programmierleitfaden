d5a9190dd141cf67a546bad4f0b0419041b347bc
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-01-07 16:35:49 +0100

    Add Franziskas review

--- listings/write_readme_doc.r
+++ listings/write_readme_doc.r
@@ -1,11 +1,11 @@
 #!/usr/bin/Rscript --vanilla
-# Write the contents of the global variable README to disk.
+# Write the contents of the global variable README to disk
 #
 # Contents of README are written to file README.txt in the 
 # directory given via the directory parameter.
 #
 # Author: Dominik Cullmann <dominik.cullmann@forst.bwl.de>
-# Version: Wed Jun  4 11:00:33 CEST 2014
+# Version: Thu Jan  7 15:48:46 CET 2016
 # Parameter: directory Path of the directory to put README.txt into.
 # Return: TRUE on success, FALSE otherwise.
 # Note: directory must exist and be writeable. No checks done.
