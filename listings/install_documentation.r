9d8a82f4094729d52a3d49d931f405f762ace299
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-10-10 08:59:42 +0200

    add new dependencies

--- listings/install_documentation.r
+++ listings/install_documentation.r
@@ -1,11 +1,10 @@
 #!/usr/bin/Rscript --vanilla
-installed_packages <- rownames(installed.packages()) 
+dependencies <- c("devtools", "httr", "backports", "Rcpp") 
+for (dependency in dependencies) if (! require(dependency, character.only = TRUE)) install.packages(dependency)
 # machines at FVAFR need to set the proxy correctly.
 if (grepl("FVAFR-",  Sys.info()["nodename"])) {
-    if (! "httr" %in% installed_packages)  install.packages("httr") 
     httr::set_config(httr::use_proxy(url="10.127.255.17", port=8080))
 }
 # documentation is a package not on CRAN, it is provided via github.
-if (! "devtools" %in% installed_packages) install.packages("devtools")
 devtools::install_github("fvafrCU/documentation")
 
